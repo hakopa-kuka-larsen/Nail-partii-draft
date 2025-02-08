17d7d628127f916e920a3abd8da51166aca30b33
hakopa-kuka-larsen <hakopasrs@gmail.com>
2025-02-08 22:45:13 +1300

    setting index, testing insta embed

---
+++ scripts/insta.js
@@ -0,0 +1,6 @@
+(function() {
+    let script = document.createElement('script');
+    script.async = true;
+    script.src = "//www.instagram.com/embed.js";
+    document.body.appendChild(script);
+})();
