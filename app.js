b49432c2d0b5f1664dcfa4f287bdb241a8e0a911
hakopa-kuka-larsen <hakopasrs@gmail.com>
2025-02-11 16:40:59 +1300

    draft 2, uneeded content removed, background image fix, navbar now sits properly in mobile version, burger menu changes color to sui background better

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const menu = document.querySelector("#mobile-menu")
-const menuLinks  = document.querySelector(".navbar__menu")
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const bars = document.querySelectorAll(".navbar__toggle .bar");
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
+menu.addEventListener("click", function () {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+
+    // Toggle .active on each bar
+    bars.forEach(bar => bar.classList.toggle("active"));
+});
+
+// let burger = document.getElementById("burger")
+
+
+
+// console.log(burger)
